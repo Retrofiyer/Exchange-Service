614d3fcb03df4c93934ed250302ab48bf5ba8a48
Sebastian <sebitas5225@gmail.com>
2024-08-24 22:20:44 -0500

    feat: :zap: Add PORT

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const swaggerDocument = require('../swagger/swagger.json');
 require('dotenv').config();
 
 const app = express();
-const port = 8103;
+const PORT = process.env.PORT || 8103;
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -15,6 +15,6 @@ app.use(cors());
 app.use('/api', currencyRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.listen(port, () => {
-  console.log(`Currency service is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Currency service is running on port ${PORT}`);
 });
